004c2f65035f616f783dcddbe543f33c489505e8
Sayana <nisayana23@yandex.ru>
2020-03-23 04:00:14 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist = {
   'Shakira': "whenever",
   'Sting': "desert rose"};
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = ["songTitle"];
+  playlist[artistName] = songTitle;
   return playlist
 }
